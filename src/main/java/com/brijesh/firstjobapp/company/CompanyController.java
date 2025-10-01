d28e9720e54bab035b9e10288bf1d683ad78802a
123Brijesh44aa <oddsworld0121@gmail.com>
2025-10-01 20:31:33 +0530

    created company model, company controller,company repository, company service and companyServiceImpl

---
+++ src/main/java/com/brijesh/firstjobapp/company/CompanyController.java
@@ -0,0 +1,55 @@
+package com.brijesh.firstjobapp.company;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+    private CompanyService companyService;
+
+    public CompanyController(CompanyService companyService){
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Company>> getAllCompanies(){
+        return new ResponseEntity<>(companyService.getAllCompanies(), HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<String> createCompany(@RequestBody Company company){
+        companyService.createCompany(company);
+        return new ResponseEntity<>("Company added successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Long id){
+        return new ResponseEntity<>(companyService.getCompanyById(id),HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCompany(@RequestBody Company company, @PathVariable Long id){
+        if (companyService.updateCompany(company,id)){
+            return new ResponseEntity<>("Company updated Successfully",HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCompany(@PathVariable Long id){
+        if (companyService.deleteCompany(id)){
+            return new ResponseEntity<>("Company Deleted successfully", HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
---
+++ src/main/java/com/brijesh/firstjobapp/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,58 @@
+package com.brijesh.firstjobapp.company.impl;
+
+import com.brijesh.firstjobapp.company.Company;
+import com.brijesh.firstjobapp.company.CompanyRepository;
+import com.brijesh.firstjobapp.company.CompanyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company updatedCompany, Long id) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (companyOptional.isPresent()){
+            Company company = companyOptional.get();
+            company.setName(updatedCompany.getName());
+            company.setDescription(updatedCompany.getDescription());
+            company.setJobs(updatedCompany.getJobs());
+            companyRepository.save(company);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public boolean deleteCompany(Long id) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (companyOptional.isPresent()){
+            companyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+}
